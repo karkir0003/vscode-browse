8e5dca8be65849f1f5c6ed96a0a37edc4e87b986
karkir0003 <karkir0003@gmail.com>
2025-05-03 11:58:02 -0400

    response.data for getting raw github user content of file

--- vscode-view-lite.user.js
+++ vscode-view-lite.user.js
@@ -2,7 +2,7 @@
 // @name         GitHub VSCode View Lite
 // @namespace    https://github.com/karkir0003/vscode-browse
 // @author       karkir0003
-// @version      0.26
+// @version      0.27
 // @description  Intercept GitHub file clicks and show inline VSCode-style viewer
 // @match        https://github.com/*/*/blob/*
 // @grant        none
@@ -64,7 +64,7 @@
       console.log("page_path: ", raw_url);
 
       //get the raw file data from the raw url
-      await fetch(raw_url).then((response) => console.log('file data: ', response));
+      await fetch(raw_url).then((response) => console.log('file data: ', response.data));
       //console.log('file data: ', file_data);
 
     });
