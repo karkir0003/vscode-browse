9f8ccf00293ee9e4dcd70f69506a255272d0ff6f
Austinmc41 <austinmc41@gmail.com>
2025-05-15 23:43:14 -0400

    duplicating section - logging to console

--- vscode-view-lite.user.js
+++ vscode-view-lite.user.js
@@ -2,7 +2,7 @@
 // @name         GitHub VSCode View Lite
 // @namespace    https://github.com/karkir0003/vscode-browse
 // @author       karkir0003
-// @version      0.60
+// @version      0.61
 // @description  Intercept GitHub file clicks and show inline VSCode-style viewer
 // @match        https://github.com/*/*/blob/*
 // @grant        none
@@ -123,9 +123,14 @@
       const raw_text = await fetch(raw_url).then((response) => response.text());
       const new_text_area = getTextArea(raw_text);
 
-      const section = document.querySelector('[aria-labelledby^="file-name-id-wide"]').cloneNode(true);
-      const section_parent = section.parentNode();
-      section_parent.appendChild(section);
+      const section = document.querySelector('[aria-labelledby^="file-name-id-wide"]');
+      console.log('section', section);
+
+      const section_clone = section.cloneNode(true);
+      console.log('section_clone', section_clone);
+
+      const section_parent = section_clone.parentNode();
+      section_parent.appendChild(section_clone);
       // // get text container children copy and append to text_parent
       // const text_parent = document.querySelector('#copilot-button-positioner');
       // const button_container = text_parent.querySelector('#copilot-button-container');
